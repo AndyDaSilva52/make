4f3cdac31866e48662e1656e2c06170989d8a934
Anderson da Silva <andydasilva52@gmail.com>
2020-03-11 23:45:33 -0300

    function
    setPropertyBetweenTwoArrays/code.js
    
    and
    
    setValueComplexObj/code.js
    
    to allow merge two array into a specific property of the collection

---
+++ apps/linx-commerce-252023/functions/setPropertyBetweenTwoArrays/code.js
@@ -0,0 +1,38 @@
+function setPropertyBetweenTwoArrays(primaryArray, secondaryArray, propertyPath, key) {
+    /**
+     * 
+     */
+    function setValueComplexObj(propertyPath, value, obj) {
+        /**
+         * 
+         */
+
+        let properties = Array.isArray(propertyPath) ? propertyPath : propertyPath.split(".")
+
+        // Not yet at the last property so keep digging
+        if (properties.length > 1) {
+            // The property doesn't exists OR is not an object (and so we overwritte it) so we create it
+            if (!obj.hasOwnProperty(properties[0]) || typeof obj[properties[0]] !== "object") obj[properties[0]] = {}
+            // We iterate.
+            return setValue(properties.slice(1), value, obj[properties[0]])
+            // This is the last property - the one where to set the value
+        } else {
+            // We set the value to the last property
+            obj[properties[0]] = value
+            return true // this is the end
+        }
+
+    }
+
+    primaryArray.forEach((item, idxPrimaryArray) => {
+
+        var idxSecondaryArray = secondaryArray.findIndex(obj => obj[key] == item[key]);
+        console.log(item['ProductID']);
+        console.log(idxSecondaryArray);
+
+        setValueComplexObj(propertyPath, secondaryArray[idxSecondaryArray], primaryArray[idxPrimaryArray]);
+
+    });
+
+    return primaryArray; // return mergedArray or change the primaryArray
+}
